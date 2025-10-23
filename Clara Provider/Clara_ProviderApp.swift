904f1239ad751b15b077b3bd8c423aa69d6efec8
dochobbs <michael@hobbs.md>
2025-10-22 22:11:33 -0500

    Initial Commit

---
+++ Clara Provider/Clara_ProviderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clara_ProviderApp.swift
+//  Clara Provider
+//
+//  Created by Michael Hobbs on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Clara_ProviderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
